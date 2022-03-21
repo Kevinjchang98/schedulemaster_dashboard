ea73ffe8f0d69ee885c5a7a8a9ae74e076cf0e07
kevinchang <kevinjchang98@gmail.com>
2022-03-21 15:29:39 -0700

    spacing of reservation elements

--- dashboard/components/HoursTilMaint/ReservationIndividual.tsx
+++ dashboard/components/HoursTilMaint/ReservationIndividual.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import styles from '../../styles/HoursTilMaint.module.css';
 
 interface Props {
     i: number;
@@ -26,36 +27,42 @@ const ReservationIndividual = ({
     };
 
     return (
-        <div key={i} style={hoursLeft < 0 ? { color: 'red' } : {}}>
+        <div
+            key={i}
+            style={hoursLeft < 0 ? { color: 'red' } : {}}
+            className={styles.reservationIndividualContainer}
+        >
             <h3
+                className={styles.reservationTitle}
                 onClick={() => {
                     expand();
                 }}
             >
                 {tailNum} - {name}
             </h3>
-
-            {isExpanded ? (
-                <>
-                    <p>{startTime.toDateString()}</p>
-                    <p>{startTime.toTimeString()}</p>
-                    <p>{endTime.toTimeString()}</p>
-                    <p>Flight length: {length.toFixed(1)} hours</p>
-                    <p>
-                        Hours left for aircraft:{' '}
-                        {(hoursLeft + length).toFixed(1)}
-                    </p>
-                    <p>
-                        Hours left if flight time is {length.toFixed(1)} hours:{' '}
-                        {hoursLeft.toFixed(1)}
-                    </p>
-                </>
-            ) : (
-                <>
-                    <p>{startTime.toDateString()}</p>
-                    <p>{startTime.toTimeString()}</p>
-                </>
-            )}
+            <div className={styles.reservationDetails}>
+                {isExpanded ? (
+                    <>
+                        <p>{startTime.toDateString()}</p>
+                        <p>{startTime.toTimeString()}</p>
+                        <p>{endTime.toTimeString()}</p>
+                        <p>Flight length: {length.toFixed(1)} hours</p>
+                        <p>
+                            Hours left for aircraft:{' '}
+                            {(hoursLeft + length).toFixed(1)}
+                        </p>
+                        <p>
+                            Hours left if flight time is {length.toFixed(1)}{' '}
+                            hours: {hoursLeft.toFixed(1)}
+                        </p>
+                    </>
+                ) : (
+                    <>
+                        <p>{startTime.toDateString()}</p>
+                        <p>{startTime.toTimeString()}</p>
+                    </>
+                )}
+            </div>
         </div>
     );
 };
