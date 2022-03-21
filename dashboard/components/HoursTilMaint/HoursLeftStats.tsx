23ebd3f9f5cec96b070166dbe64af0170c4e30bf
kevinchang <kevinjchang98@gmail.com>
2022-03-21 14:33:44 -0700

    css styling, add fade in

--- dashboard/components/HoursTilMaint/HoursLeftStats.tsx
+++ dashboard/components/HoursTilMaint/HoursLeftStats.tsx
@@ -1,4 +1,6 @@
 import { DocumentData, QueryDocumentSnapshot } from 'firebase/firestore';
+import FadeIn from 'react-fade-in/lib/FadeIn';
+import styles from '../../styles/HoursTilMaint.module.css';
 
 interface Props {
     isLoaded: boolean;
@@ -7,29 +9,37 @@ interface Props {
 
 const HoursLeftStats = ({ isLoaded, aircraftData }: Props) => {
     return (
-        <div>
-            <h1>Aircraft</h1>
-            {!isLoaded ? (
-                <p>Loading</p>
-            ) : aircraftData.length == 0 ? (
-                <div>No aircraft data available</div>
-            ) : (
-                aircraftData.map((aircraft: any, i: number) => (
-                    <div key={i}>
-                        <h3>{aircraft.data().tail_num}</h3>
-                        <p
-                            style={
-                                aircraft.data().hours_remaining < 10
-                                    ? { color: 'red' }
-                                    : {}
-                            }
-                        >
-                            {aircraft.data().hours_remaining} hours left
-                        </p>
-                    </div>
-                ))
-            )}
-        </div>
+        <>
+            <h1 className={styles.subSectionTitle}>Aircraft</h1>
+            <div className={styles.subSectionContentsContainer}>
+                {!isLoaded ? (
+                    <FadeIn>
+                        <p>Loading</p>
+                    </FadeIn>
+                ) : aircraftData.length == 0 ? (
+                    <FadeIn>
+                        <div>No aircraft data available</div>
+                    </FadeIn>
+                ) : (
+                    aircraftData.map((aircraft: any, i: number) => (
+                        <FadeIn delay={200}>
+                            <div key={i}>
+                                <h3>{aircraft.data().tail_num}</h3>
+                                <p
+                                    style={
+                                        aircraft.data().hours_remaining < 10
+                                            ? { color: 'red' }
+                                            : {}
+                                    }
+                                >
+                                    {aircraft.data().hours_remaining} hours left
+                                </p>
+                            </div>
+                        </FadeIn>
+                    ))
+                )}
+            </div>
+        </>
     );
 };
 
