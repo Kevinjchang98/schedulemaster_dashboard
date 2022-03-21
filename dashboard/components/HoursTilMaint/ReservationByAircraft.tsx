95981e64e22b1593f86a094b15cbec1241c5d0db
kevinchang <kevinjchang98@gmail.com>
2022-03-20 19:14:05 -0700

    add header for each reservation section

--- dashboard/components/HoursTilMaint/ReservationByAircraft.tsx
+++ dashboard/components/HoursTilMaint/ReservationByAircraft.tsx
@@ -44,23 +44,32 @@ const ReservationByAircraft = ({
         }
     }
 
-    return filteredData.map((schedule: any, i: number) => {
-        let startTime = new Date(schedule.sch_start);
-        let endTime = new Date(schedule.sch_end);
+    return (
+        <>
+            <h2>{aircraftTailNum}</h2>
+            <h3>{filteredData.length} reservations</h3>
+            {filteredData.map((schedule: any, i: number) => {
+                let startTime = new Date(schedule.sch_start);
+                let endTime = new Date(schedule.sch_end);
 
-        return (
-            <div key={i} style={hoursLeft[i] < 0 ? { color: 'red' } : {}}>
-                <ReservationIndividual
-                    i={i}
-                    hoursLeft={hoursLeft[i]}
-                    tailNum={schedule.N_NO}
-                    name={schedule.NAME}
-                    startTime={startTime}
-                    endTime={endTime}
-                />
-            </div>
-        );
-    });
+                return (
+                    <div
+                        key={i}
+                        style={hoursLeft[i] < 0 ? { color: 'red' } : {}}
+                    >
+                        <ReservationIndividual
+                            i={i}
+                            hoursLeft={hoursLeft[i]}
+                            tailNum={schedule.N_NO}
+                            name={schedule.NAME}
+                            startTime={startTime}
+                            endTime={endTime}
+                        />
+                    </div>
+                );
+            })}
+        </>
+    );
 };
 
 export default ReservationByAircraft;
