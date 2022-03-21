b5c73cc668f22125c4331b39db7cdcd0d88aed03
kevinchang <kevinjchang98@gmail.com>
2022-03-21 15:34:03 -0700

    move font import to css file

--- dashboard/pages/index.tsx
+++ dashboard/pages/index.tsx
@@ -15,10 +15,6 @@ const Home: NextPage = () => {
 
                 {/* TODO: Change icon */}
                 <link rel="icon" href="/favicon.ico" />
-                <style>
-                    @import
-                    url("https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200;400&display=swap");
-                </style>
             </Head>
 
             <main className={styles.main}>
